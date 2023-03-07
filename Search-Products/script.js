00a59ea8c8828a931c1b7fb511c3a64a09bbe627
shseok <shseok0674@gmail.com>
2023-03-07 14:59:00 +0900

    Update Search Product

--- Search-Products/script.js
+++ Search-Products/script.js
@@ -3,6 +3,25 @@ const $form = document.querySelector('form');
 const $Btn = $form.querySelector('#submit-button');
 const $products = document.querySelector('.product-list');
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    fetch('./products.json').then((rs) => rs.json()).then(({results}) =>{
+        results.forEach((each) => {
+            const $product = document.createElement('div');
+            $product.classList.add('product')
+            const $details = document.createElement('div');
+            $details.classList.add('details')
+            const $name = document.createElement('h2');
+            const $price = document.createElement('h3');
+            $name.textContent = each.productName;
+            $price.textContent = each.price;
+            $details.append($name, $price);
+            $product.append($details);
+            $products.append($product);
+        });
+    });
+    console.log($products)
+});
+
 $form.addEventListener('submit', (e) => {
     e.preventDefault();
     if(!$input.value) return;
@@ -13,5 +32,17 @@ $form.addEventListener('submit', (e) => {
             $products.children[i].style.display = 'none';
         }
     }
-    // $input.value = '';
 });
+
+$input.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter'){
+        if(!$input.value){
+            for(let i = 0; i < $products.children.length; i++){
+                $products.children[i].style.display = 'block';
+            }
+        }else{
+            $Btn.click();
+        }
+    }
+
+})
